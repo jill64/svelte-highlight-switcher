cd5953fbdec7eddafedfce45fb2ff2f4538c8a54
jill64 <intents.turrets0h@icloud.com>
2023-12-04 20:26:14 +0900

    def lib

---
+++ src/lib/HighlightSwitcher.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import * as styles from 'svelte-highlight/styles'
+  import type { StyleName } from './types/StyleName'
+
+  export let name: StyleName
+</script>
+
+<svelte:head>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html styles[name]}
+</svelte:head>
---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+export { default as HighlightSwitcher } from './HighlightSwitcher.svelte'
+export type { StyleName } from './types/StyleName'
